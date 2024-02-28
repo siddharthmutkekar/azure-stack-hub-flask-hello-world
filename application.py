637103adde2520af204f1b8f03fee28efc09468c
Siddharth S Mutkekar <siddharthmutkekar@in.ibm.com>
2024-02-28 12:14:41 +0530

    updated

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello siddharth s Updated World!"
+    return "Hello siddh Updated World!"
 
 if __name__ == '__main__':
     app.run()
